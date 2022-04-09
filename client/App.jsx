546b979e2dc21d55433bb2f0f4acc585aed083f4
james-b-olivas <jamesbolivas@gmail.com>
2022-04-08 18:00:04 -0700

    Fix bug related to checking valid slow time metric

--- client/App.jsx
+++ client/App.jsx
@@ -9,7 +9,7 @@ class App extends Component {
       stats: [],
       slowPickupCount: 0,
       slowPreOrderboardCount: 0,
-      slowTimeMetric: 100,
+      slowTimeMetric: '',
       mostRecentSlowTime: 100,
       currentDay: 22,
       tempCurrentDay: '22'
@@ -17,22 +17,11 @@ class App extends Component {
     this.handleTextChange = this.handleTextChange.bind(this);
     this.handleFileChange = this.handleFileChange.bind(this);
     this.setCurrentDay = this.setCurrentDay.bind(this);
-    this.submitCurrentDay = this.submitCurrentDay.bind(this);
   }
 
   setCurrentDay(e) {
     e.preventDefault();
     this.setState({tempCurrentDay: e.target.value});
-  }
-
-  submitCurrentDay(e) {
-    e.preventDefault();
-    let val = parseInt(this.state.tempCurrentDay);
-    if (isNaN(val) || val < 22 || val > 29) {
-      alert("Please input a proper value between 22 and 29");
-      return;
-    }
-    this.setState({currentDay: val});
   }
 
   handleTextChange(e) {
@@ -43,12 +32,18 @@ class App extends Component {
 
   handleFileChange(e) {
     e.preventDefault();
-    if (isNaN(this.state.slowTimeMetric)) {
-      alert("Please input a numeric value!");
+    if (isNaN(this.state.slowTimeMetric) || this.state.slowTimeMetric.length === 0) {
+      alert("Please input a numeric time value!");
+      return;
+    }
+    let val = parseInt(this.state.tempCurrentDay);
+    if (isNaN(val) || val < 22 || val > 29) {
+      alert("Please input a proper date between March 22 and March 29!");
       return;
     }
     this.setState({
-      mostRecentSlowTime: this.state.slowTimeMetric
+      mostRecentSlowTime: this.state.slowTimeMetric,
+      currentDay: val
     })
     $.get('/api/csv', (data) => {
       console.log(data);
@@ -82,18 +77,18 @@ class App extends Component {
           <label>
             Input a value of time: how slow is too slow for a customer to wait?
             <div>
-              <input type="text" value={this.state.slowTimeMetric} onChange={this.handleTextChange}></input>
+              <input type="text" onChange={this.handleTextChange}></input>
             </div>
           </label>
           <label>
             Choose a day between March 22 and March 29
             <div>
               March <input type="text" value={this.state.tempCurrentDay} onChange={this.setCurrentDay}/>
-              <button onClick={this.submitCurrentDay}>Enter Date</button>
+              {/* <button onClick={this.submitCurrentDay}>Enter Date</button> */}
             </div>
           </label>
           <form>
-            <button onClick={this.handleFileChange}>Process Data For March {this.state.currentDay}</button>
+            <button onClick={this.handleFileChange}>Process Data</button>
           </form>
           <div>
             {this.state.hasFile ? <div>
